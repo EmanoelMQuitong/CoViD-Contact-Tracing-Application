29e0fa85264baa906e0f2ba4a5fa0b8f7166c8e6
EmanoelMQuitong <emanoelquitong23@gmail.com>
2023-07-24 21:43:57 +0800

    second page has beend successfully created. Personal Information page works successfully. Will move on the Back and Next button functions. After that, Health background page will be initiated.

--- CT-App-Organized.py
+++ CT-App-Organized.py
@@ -1,15 +1,20 @@
 import tkinter as tk
 from tkinter import messagebox
+
 class CCT:
 
     def __init__(self):
-        self.root = tk.Tk()
 
+
+        self.welcome = tk.Tk()
+        
         #Create a main frame
-        self.root.geometry("500x500")
-        self.root.title("Covid Contact Tracing Application")
+        self.welcome.geometry("500x500")
+        self.welcome.title("Covid Contact Tracing Application")
+        self.welcome.resizable(False,False)
 
-        self.Main_frame = tk.Frame(self.root, )
+
+        self.Main_frame = tk.Frame(self.welcome)
 
         self.Main_frame.pack(fill="both", expand=1)
 
@@ -49,20 +54,22 @@ class CCT:
         self.check_agree = tk.IntVar()
 
         #Create Agreement checkbox button
-        self.agree=tk.Checkbutton(self.root, text=" I have read and understood the information above. I hereby agree and give consent to use my personal information by the stated purposes only. ", justify="left",wraplength= "400", font=("Arial", 8, "underline"), variable=self.check_agree)
+        self.agree=tk.Checkbutton(self.welcome, text=" I have read and understood the information above. I hereby agree and give consent to use my personal information by the stated purposes only. ", justify="left",wraplength= "400", font=("Arial", 8, "underline"), variable=self.check_agree)
         self.agree.pack(side='left',padx=10, pady=10, expand=1, fill="both")
 
         #Add next button
-        self.nextbutton = tk.Button(self.root, text="Next >",font=("Arial",12), command=self.move_to_next1)
+        self.nextbutton = tk.Button(self.welcome, text="Next >",font=("Arial",12), command=self.move_to_next1)
         self.nextbutton.pack(side='top', padx=10, pady=10,fill="both")
 
-        self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
-        self.root.mainloop()
+        self.welcome.protocol("WM_DELETE_WINDOW", self.on_closing)
+        self.welcome.mainloop()
 
+    #a function that asks the user when close window is clicked
     def on_closing(self):
         if messagebox.askyesno(title="Are you sure?", message="Do you really want to quit?"):
-            self.root.destroy()
+            self.welcome.destroy()
 
+    #open a file then read all of its contents
     def read_file(self, cf):
         try:
             with open(cf, 'r') as file:
@@ -74,16 +81,34 @@ class CCT:
         except IOError:
             print(f'Error reading file: {cf}')
 
+    #Command to move to next window when "next >" is clicked
     def move_to_next1(self):
         if self.check_agree.get()==0:
             messagebox.showinfo(title="To Proceed", message="Please click the check box before you proceed.")
         else:
-            self.PI = tk.Toplevel(self.root)
+            #Hide the welcome window
+            self.welcome.withdraw()
+            
+            #Create new window
+            self.PI = tk.Toplevel(self.welcome, bg='lemon chiffon', relief = "sunken", bd=5)
             self.PI.geometry("500x500")
             self.PI.title("Personal Information")
 
+            #Set up second frame
+            self.sec_frame = tk.Frame(self.PI)
+            self.sec_frame.pack(side="top", fill="both",)
 
-            self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
+            Name=tk.StringVar()
+            Age=tk.StringVar()
+            Status=tk.StringVar()
+            Address=tk.StringVar()
+            Room=tk.StringVar()
+
+            entry_Name=tk.Entry()
+
+
+
+            self.PI.protocol("WM_DELETE_WINDOW", self.on_closing)
             self.PI.mainloop()
 
 
